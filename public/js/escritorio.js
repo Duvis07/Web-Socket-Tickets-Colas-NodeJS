611eb9bfec3cbe5ca7489ef114fe1144718675fe
duvan.botero <duvis07>
2022-12-20 14:04:35 -0500

    Se muestra los tickets por pantalla

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,44 @@
+// refencia al html para poder manipularlo
 
+const lblEscritorio = document.querySelector("h1");
+const btnAtender = document.querySelector("button");
+const lblNuevoTicket = document.querySelector("small");
+const divAlerta = document.querySelector(".alert");
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error("El escritorio es necesario");
+}
+
+const escritorio = searchParams.get("escritorio");
+lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = "none";
+
+const socket = io();
+
+socket.on("connect", () => {
+  btnAtender.disabled = false;
+});
+
+socket.on("disconnect", () => {
+  btnAtender.disabled = true;
+});
+
+socket.on("ultimo-ticket", (ultimo) => {
+  /*  lblNuevoTicket.innerText = "Ticket " + ultimo; */
+});
+
+btnAtender.addEventListener("click", () => {
+  socket.emit("atender-ticket", { escritorio }, ({ ok, ticket, msg }) => {
+    if (!ok) {
+      lblNuevoTicket.innerText = "Nadie";
+      return (divAlerta.style.display = "");
+    }
+
+    lblNuevoTicket.innerText = "Ticket " + ticket.numero;
+  }
+    );
+});
--- sockets/controllers.js
+++ sockets/controllers.js
@@ -5,13 +5,39 @@ const ticketControl = new TicketControl();
 const socketController = (socket) => {
   // Muestra el ultimo ticket que se atendio en pantalla
   socket.emit("ultimo-ticket", ticketControl.ultimo);
+  // Muestra los ultimos 4 tickets que se atendieron en pantalla
+  socket.emit("estado-actual", ticketControl.ultimos4);
 
   socket.on("siguiente-ticket", (payload, callback) => {
     const siguiente = ticketControl.siguiente();
     callback(siguiente);
   });
 
-  //Notificar que hay un nuevo ticket pendiente de atender en los escritorios
+  socket.on("atender-ticket", ({ escritorio }, callback) => {
+    if (!escritorio) {
+      return callback({
+        ok: false,
+        msg: "El escritorio es obligatorio",
+      });
+    }
+
+    const ticket = ticketControl.atenderTicket(escritorio);
+
+    // Actualizar / notificar cambios en los ultimos 4
+    socket.broadcast.emit("estado-actual", ticketControl.ultimos4);
+
+    if (!ticket) {
+      return callback({
+        ok: false,
+        msg: "Ya no hay tickets pendientes",
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
+  });
 };
 
 module.exports = {
