846cf4f4ac00fb149dcf0d1a220b0c1af7bc08ab
duvan.botero <duvis07>
2022-12-20 11:31:39 -0500

    Se crea metodo para generar tickets

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,3 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+//Clase para tener un control de los tickets que se van generando y atendiendo en el sistema
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero;
+    this.escritorio = escritorio;
+  }
+}
+
+//Clase para manejar los tickets
 class TicketControl {
   constructor() {
     this.ultimo = 0;
@@ -7,5 +19,80 @@ class TicketControl {
     this.tickets = [];
 
     this.ultimos4 = [];
+
+    this.init();
   }
+
+  //Lo que va a guardar un objeto en la base de datos en este caso un archivo json
+  get toJson() {
+    return {
+      ultimo: this.ultimo,
+      hoy: this.hoy,
+      tickets: this.tickets,
+      ultimos4: this.ultimos4,
+    };
+  }
+
+  //Metodo para inicializar el servidor o la clase
+  init = () => {
+    const { hoy, tickets, ultimo, ultimos4 } = require("../db/data.json");
+
+    if (hoy === this.hoy) {
+      this.tickets = tickets;
+      this.ultimo = ultimo;
+      this.ultimos4 = ultimos4;
+    } else {
+      //Es otro dia  reiniciar todas las variables
+      this.guardarDB();
+    }
+  };
+
+  //Metodo para guardar en la base de datos los tickets
+  guardarDB = () => {
+    const dbPath = path.join(__dirname, "../db/data.json");
+    // convierte un string a un objeto json
+    fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+  };
+
+  //Metodo para obtener el siguiente ticket a atender
+  siguiente = () => {
+    this.ultimo += 1;
+    const ticket = new Ticket(this.ultimo, null);
+    this.tickets.push(ticket);
+
+    this.guardarDB();
+
+    return 'Ticket ' + ticket.numero;
+  };
+
+  //Metodo para atender un ticket  y asignarlo a un escritorio
+  //Puede retornar null si no hay tickets en la cola o va a retornar el ticket que se esta atendiendo en este momento
+  atenderTicket = (escritorio) => {
+    //No hay tickets en la cola  no hay tickets para atender
+    if (this.tickets.length === 0) {
+      return null;
+    }
+    //shift() saca el primer elemento del arreglo y lo retorna y lo elimina del arreglo
+    const ticket = this.tickets.shift();
+
+    //Asignar el escritorio al ticket que se esta atendiendo en este momento
+    ticket.escritorio = escritorio;
+
+    // unshift Agrega el ticket al inicio del arreglo de ultimos 4 tickets
+    this.ultimos4.unshift(ticket);
+
+    //Verificar que solo existan 4 tickets en el arreglo de ultimos 4 tickets
+    if (this.ultimos4.length > 4) {
+      //borra el ultimo elemento del arreglo y lo corta en 1 o  el ultimo elemento
+      this.ultimos4.splice(-1, 1);
+    }
+
+    //Guardar en la base de datos
+    this.guardarDB();
+
+    //Retornar el ticket que se esta atendiendo en este momento para que se muestre en el html
+    return ticket;
+  };
 }
+
+module.exports = TicketControl;
--- sockets/controllers.js
+++ sockets/controllers.js
@@ -1,14 +1,17 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
 const socketController = (socket) => {
+  // Muestra el ultimo ticket que se atendio en pantalla
+  socket.emit("ultimo-ticket", ticketControl.ultimo);
 
-  socket.on("disconnect", () => {
+  socket.on("siguiente-ticket", (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
   });
 
-  // Escuchar evento "send-message" y emitirlo a todos los clientes conectados
-  socket.on("send-message", (payload, callback) => {
-    const id = 123456789;
-    callback({ id, date: new Date().getTime() });
-    socket.broadcast.emit("send-message", payload);
-  });
+  //Notificar que hay un nuevo ticket pendiente de atender en los escritorios
 };
 
 module.exports = {
